852cdf1dc363cf9012b2c44dbff10801cdb4b5a9
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-06-01 21:51:59 -0500

    mejoras en las traducciones

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -20,13 +20,12 @@
                     d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
             </svg>
             <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">
-                <a href="https://laravel.com/docs">Documentation</a>
+                <a href="https://laravel.com/docs">{{ __('Documentation') }}</a>
             </h2>
         </div>
 
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-            Laravel has wonderful documentation covering every aspect of the framework. Whether you're new to the
-            framework or have previous experience, we recommend reading all of the documentation from beginning to end.
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed text-justify">
+            {{ __('DocumentationParagraph') }}
         </p>
 
         <p class="mt-4 text-sm">
@@ -56,9 +55,8 @@
             </h2>
         </div>
 
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-            Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out,
-            see for yourself, and massively level up your development skills in the process.
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed text-justify">
+            {{ __('LaracastsParagraph') }}
         </p>
 
         <p class="mt-4 text-sm">
@@ -88,10 +86,8 @@
             </h2>
         </div>
 
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-            Laravel Jetstream is built with Tailwind, an amazing utility first CSS framework that doesn't get in your
-            way. You'll be amazed how easily you can build and maintain fresh, modern designs with this wonderful
-            framework at your fingertips.
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed text-justify">
+            {{ __('TailwindParagraph') }}
         </p>
     </div>
 
@@ -103,14 +99,12 @@
                     d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
             </svg>
             <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">
-                Authentication
+                {{ __('Authentication') }}
             </h2>
         </div>
 
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-            Authentication and registration views are included with Laravel Jetstream, as well as support for user email
-            verification and resetting forgotten passwords. So, you're free to get started with what matters most:
-            building your application.
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed text-justify">
+            {{ __('AuthenticationParagraph') }}
         </p>
     </div>
 </div>
